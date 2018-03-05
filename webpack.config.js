d52617155afd0a71ce72aadfceee12296ccb3ca7
janpauldahlke <janpauldahlke@gmail.com>
2018-03-05 17:14:15 +0100

    mvp boiler

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
